d9fd7ee17252a8fe3cce4aab43c7743ce3995cce
yusuke39 <hiranoyusuke39@gmail.com>
2021-05-23 14:11:04 +0900

    buttonのCSSをオブジェクト化してみる

--- src/share/Button.js
+++ src/share/Button.js
@@ -1,5 +1,18 @@
-const Button = (props: [String]) => {
-  return <button>{props.buttonName}</button>;
+import "../css/share/Button.css";
+
+const Button = (props) => {
+  return (
+    <div>
+      <button style={buttonStyle.button1}>{props.buttonName}</button>
+    </div>
+  );
+};
+
+var buttonStyle = {
+  button1: {
+    color: "#ffffff",
+    backgroundColor: "blue",
+  },
 };
 
 export default Button;
